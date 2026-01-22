8a72859f4ba7e822bfdcd9321508f9c0f82de0fb
xovar <xovar9663@gmail.com>
2026-01-22 13:13:46 +0600

    add error middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 import connectDB from "./database/connectDB.js";
+import errorMiddleware from "./middleware/error.middlewares.js";
 
 const app = express();
 
 const port =  PORT || 8000;
+
+
+app.use(errorMiddleware)
 
 app.listen(port, () => {
     connectDB()
